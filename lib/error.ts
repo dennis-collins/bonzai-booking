19fd619ff17d0902b02ebd8b83c7788d6d0757ce
K1rr1 <kiriakos.it.karipidis@gmail.com>
2025-09-17 12:26:05 +0200

    DDB+Error
    Tog bort db.jms gjorde en ny ddb.ts fil + error fil med olika error classer

---
+++ lib/error.ts
@@ -0,0 +1,60 @@
+
+// bas klass för alla app fel, hur felen ska hanteras för och visas i http-respons
+export class AppError extends Error {
+  statusCode: number;
+  details?: any;
+
+  constructor(message: string, statusCode = 400, details?: any) {
+    super(message);// anropa bas klass konstruktor
+    this.statusCode = statusCode;// http status kod
+    this.details = details; // extra info om fel (valfritt)
+  }
+}
+
+
+// felklass för valideringsfel
+export class ValidationError extends AppError {
+  constructor(message: string, details?: any) {
+    super(message, 400, details);
+    this.name = "ValidationError";
+  }
+}
+// felklass för icke funnet fel
+export class NotFoundError extends AppError {
+  constructor(message: string) {
+    super(message, 404);
+    this.name = "NotFoundError";
+  }
+}
+// felklass för interna server fel
+export class InternalError extends AppError {
+  constructor(message = "Internal Server Error") {
+    super(message, 500);
+    this.name = "InternalError";
+  }
+}
+
+// Hjälpfunktion: konvertera error objectet till HTTP-respons
+export function errorResponse(err: unknown) {
+  if (err instanceof AppError) {
+    return {
+      statusCode: err.statusCode,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        error: err.name,
+        message: err.message,
+        details: err.details,
+      }),
+    };
+  }
+
+  // om okänt fel ger -> 500
+  return {
+    statusCode: 500,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      error: "InternalError",
+      message: (err as any)?.message || "Unexpected error",
+    }),
+  };
+}
