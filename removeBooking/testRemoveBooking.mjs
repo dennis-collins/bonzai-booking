38e4d39fafd3c53c9bc6becfe4ba3a57349ea4bb
AdrianRydin <adrian.rydin@gmail.com>
2025-09-18 18:38:24 +0200

    Fixed removeBooking
    Also changed some small things in createBooking, more specifically fixed a typo in bookingID to bookingId

---
+++ removeBooking/testRemoveBooking.mjs
@@ -0,0 +1,24 @@
+// testRemoveBooking.mjs
+import { handler } from "./index.mjs";
+
+// Mock-event som liknar det API Gateway skickar
+const event = {
+  pathParameters: {
+    bookingId: "1GT96X", // <-- ändra till ett ID som finns i din DynamoDB-tabell
+  },
+  body: JSON.stringify({
+    guestEmail: "alice@example.com", // valfritt, kan tas bort om du inte använder condition
+  }),
+};
+
+const run = async () => {
+  try {
+    const res = await handler(event);
+    console.log("Lambda response:");
+    console.log(res);
+  } catch (err) {
+    console.error("Lambda threw an error:", err);
+  }
+};
+
+run();
